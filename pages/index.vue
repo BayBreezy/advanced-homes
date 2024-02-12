bd8e2f22990066636f9627e0e7b52489d13d8d9f
BayBreezy <behon.baker@yahoo.com>
2024-02-11 20:53:56 -0500

    chore: format code

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,14 @@
 <template>
-	<div class="min-h-screen relative">
-		<img
-			src="/home.jpg"
-			alt="Advance Home"
-			class="object-cover h-full w-full absolute inset-0 -z-0"
-		/>
-		<div
-			class="absolute inset-0 bg-gradient-to-b from-white via-white/60 to-transparent"
-		></div>
-		<div class="h-full relative">
-			<Header />
-			<Hero />
-		</div>
-	</div>
+  <div class="min-h-screen relative">
+    <img
+      src="/home.jpg"
+      alt="Advance Home"
+      class="object-cover h-full w-full absolute inset-0 -z-0"
+    />
+    <div class="absolute inset-0 bg-gradient-to-b from-white via-white/60 to-transparent"></div>
+    <div class="h-full relative">
+      <Header />
+      <Hero />
+    </div>
+  </div>
 </template>
-
-<style></style>
