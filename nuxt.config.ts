b6a07a1d3278b946a897e72531f05837fd49d0b6
BayBreezy <behon.baker@yahoo.com>
2024-02-11 20:53:38 -0500

    chore: remove explicit import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
-import { defineNuxtConfig } from "nuxt3";
-
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-	css: ["~/assets/css/main.css"],
-	postcss: {
-		plugins: {
-			tailwindcss: {},
-			autoprefixer: {},
-		},
-	},
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
