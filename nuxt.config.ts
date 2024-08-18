4838949606662a254f33877fe38d092e83aded00
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2024-08-18 17:43:23 -0500

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  compatibilityDate: "2024-08-18",
 });
